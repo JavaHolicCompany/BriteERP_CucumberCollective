78235ea007958ed78c620de3bdf7895584957f56
JavaHolicCompany <47796511+JavaHolicCompany@users.noreply.github.com>
2019-02-20 20:01:56 -0500

    Update CukesRunner.java

--- src/test/java/runner/CukesRunner.java
+++ src/test/java/runner/CukesRunner.java
@@ -12,7 +12,6 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue="stepDefinitions",
         dryRun = false,
-        tags = "@pm"
 )
 public class CukesRunner{
 
